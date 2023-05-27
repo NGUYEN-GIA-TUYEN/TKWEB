2bf3fcbeeeaa24f40ef606da8be05a6f8ee62481
NGUYEN-GIA-TUYEN <117697739+NGUYEN-GIA-TUYEN@users.noreply.github.com>
2023-05-27 20:46:56 +0700

    Update JS05.js

--- BAI03/JS05.js
+++ BAI03/JS05.js
@@ -1,1 +1,27 @@
+class TaiKhoanNganHang {
+  constructor(soDuBanDau) {
+    this.soDu = soDuBanDau;
+  }
 
+  guiTien(amount) {
+    this.soDu += amount;
+  }
+
+  rutTien(amount) {
+    if (amount > this.soDu) {
+      console.log("Số dư không đủ");
+    } else {
+      this.soDu -= amount;
+    }
+  }
+
+  kiemTraSoDu() {
+    console.log(`Số dư hiện tại: ${this.soDu}`);
+  }
+}
+
+let taiKhoan = new TaiKhoanNganHang(1000);
+
+taiKhoan.guiTien(500); // Gửi thêm 500 vào tài khoản
+taiKhoan.rutTien(200); // Rút 200 khỏi tài khoản
+taiKhoan.kiemTraSoDu(); // In ra số dư hiện tại
