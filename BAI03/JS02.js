6c64972f63c40dd081c80809f6ceda93626cee54
NGUYEN-GIA-TUYEN <117697739+NGUYEN-GIA-TUYEN@users.noreply.github.com>
2023-05-27 20:35:26 +0700

    Update JS02.js

--- BAI03/JS02.js
+++ BAI03/JS02.js
@@ -1,1 +1,12 @@
+function findMax(arr) {
+let max = arr[0];
+for (let i = 1; i < arr.length; i++) {
+if (arr[i] > max) {
+max = arr[i];
+}
+}
+return max;
+}
 
+// Ví dụ sử dụng
+console.log(timGiaTriLonNhat([5, 8, 3, 2, 9])); // Kết quả: 9
